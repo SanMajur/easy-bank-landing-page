ecd1724aff06e5a624441037d6be706c4b1aee56
santino majur <demalong22@gmail.com>
2023-03-07 23:12:18 +0300

    Added toggle to hamburger menu

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("yuhhhhhh")
+const hamburgerBtn = document.querySelector('#mobile-menu');
+const imgToggle = document.querySelector('#hamburger-img')
+
+
+let toggle = true;
+hamburgerBtn.addEventListener('click', () => {
+    toggle = !toggle;
+    if(toggle) {
+        imgToggle.src = './images/icon-hamburger.svg'
+    } else {
+        imgToggle.src = './images/icon-close.svg'
+    }
+})
