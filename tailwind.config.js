030472333d1478fc25c4df287813a5226ad21a5e
santino majur <demalong22@gmail.com>
2023-03-08 16:40:23 +0300

    Responsive hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      // padding: {
+      //   sm: '2rem',
+      //   lg: '4rem',
+      //   xl: '5rem',
+      //   '2xl': '6rem',
+      // }
+    },
     extend: {
       colors: {
         'darkBlue': 'hsl(233, 26%, 24%)',
@@ -11,7 +20,7 @@ module.exports = {
         'lightGrayishBlue': 'hsl(220, 16%, 96%)',
         'lightGray': 'hsl(0, 0%, 98%)',
         'lightWhite': 'hsl(0, 0%, 100%)'
-      }
+      },
     },
   },
   plugins: [],
