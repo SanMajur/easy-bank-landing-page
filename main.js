3eb683dc1a328f6f4c19195669b956f22055ddc4
santino majur <demalong22@gmail.com>
2023-03-10 10:21:55 +0300

    Responsive navbar with mobile toggle

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const hamburgerBtn = document.querySelector('#mobile-menu');
 const imgToggle = document.querySelector('#hamburger-img')
-
+const navbar = document.querySelector('nav')
 
 let toggle = true;
 hamburgerBtn.addEventListener('click', () => {
     toggle = !toggle;
     if(toggle) {
         imgToggle.src = './images/icon-hamburger.svg'
+        navbar.classList.remove('open')
     } else {
         imgToggle.src = './images/icon-close.svg'
+        navbar.classList.add('open')
     }
 })
